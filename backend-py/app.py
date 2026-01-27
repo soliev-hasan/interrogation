82818c4baa7098cc1daf5c73f6f2f6bc61039160
burhon97 <idriskhonovdev97@gmail.com>
2026-01-27 16:37:39 +0500

    minor fix

--- backend-py/app.py
+++ backend-py/app.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Speech to Text API")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
